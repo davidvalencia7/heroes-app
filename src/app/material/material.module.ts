93c8c17f6ecf14a02776e494e25f8bf33e33dbec
Deivid <davidvalenciamontes7@gmail.com>
2022-05-16 14:59:41 -0500

    207.- Diseño de la pantalla para agregar heroes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import {MatListModule} from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatGridListModule} from '@angular/material/grid-list'; 
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select'; 
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
@@ -25,6 +26,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatListModule,
     MatGridListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule,
   ],
